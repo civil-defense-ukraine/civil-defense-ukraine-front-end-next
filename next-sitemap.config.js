6a45f5f53bf79d41464bc7d6e01ab0d41cc82f0f
Tetiana Bukhenko <tania04bukhenko@gmail.com>
2025-05-11 21:34:28 +0300

    add improved sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,13 @@
-/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://civil-defense-ukraine.com', 
+  siteUrl: 'https://www.civil-defense-ukraine.com',
   generateRobotsTxt: true,
-  sitemapSize: 7000,
-  outDir: './public',
-};
+  additionalPaths: async () => {
+    const res = await fetch('https://cdu-backend-service-latest-am7e.onrender.com/api/public/news');
+    const news = await res.json();
+
+    return news.map(newsItem => ({
+      loc: `https://www.civil-defense-ukraine.com/news/${newsItem.id}`,
+      lastmod: newsItem.lastUpdated,
+    }));
+  },
+}
