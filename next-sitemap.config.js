9a9cd8732142250bbdbfffe4a3f22b4aa4bff6c2
Tetiana Bukhenko <tania04bukhenko@gmail.com>
2025-06-15 23:26:51 +0300

    feat: update sitemap generation to include static URLs and improve news URL mapping

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,10 +4,22 @@ module.exports = {
   additionalPaths: async () => {
     const res = await fetch('https://cdu-backend-service-latest-am7e.onrender.com/api/public/news');
     const news = await res.json();
+    const staticUrls = [
+      { loc: 'https://www.civil-defense-ukraine.com/' },
+      { loc: 'https://www.civil-defense-ukraine.com/about-us' },
+      { loc: 'https://www.civil-defense-ukraine.com/donate' },
+      { loc: 'https://www.civil-defense-ukraine.com/news' },
+      { loc: 'https://www.civil-defense-ukraine.com/reports' },
+    ];
 
-    return news.map(newsItem => ({
+    const newsUrls = news.map(newsItem => ({
       loc: `https://www.civil-defense-ukraine.com/news/${newsItem.id}`,
       lastmod: newsItem.lastUpdated,
     }));
+
+    return [
+      ...staticUrls,
+      ...newsUrls,
+    ];
   },
 }
